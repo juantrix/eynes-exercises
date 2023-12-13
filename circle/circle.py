4d14f2d25d48a7d07a1c5e17863b70a7ce95bd62
juan <juan.i@genosha.com.ar>
2023-12-13 17:03:01 -0300

    set_radius function implemented

--- circle/circle.py
+++ circle/circle.py
@@ -3,9 +3,14 @@ import math
 
 class Circle():
     def __init__(self, radius):
-        if radius < 0:
+        if radius <= 0:
             raise ValueError("Radius cannot be negative or zero")
         self.radius = radius
 
     def get_radius(self):
         return self.radius
+
+    def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError("Radius cannot be negative or zero")
+        self.radius = radius
